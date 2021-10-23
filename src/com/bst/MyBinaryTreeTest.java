39e3e0ad2f11d32d7d7defd0deadde8f7d4be3d4
SHAILESH <shaileshdubeyr@gmail.com>
2021-10-24 02:47:15 +0530

    Refactor : code for added remain child and leaf node

--- src/com/bst/MyBinaryTreeTest.java
+++ src/com/bst/MyBinaryTreeTest.java
@@ -11,7 +11,18 @@ public class MyBinaryTreeTest {
 		myBinaryTree.add(56);
 		myBinaryTree.add(30);
 		myBinaryTree.add(70);
+		myBinaryTree.add(11);
+		myBinaryTree.add(3);
+		myBinaryTree.add(40);
+		myBinaryTree.add(22);
+		myBinaryTree.add(16);
+		myBinaryTree.add(70);
+		myBinaryTree.add(67);
+		myBinaryTree.add(63);
+		myBinaryTree.add(60);
+		myBinaryTree.add(65);
+		myBinaryTree.add(95);
 		int size = myBinaryTree.getSize();
-		Assert.assertEquals(3, size);
+		Assert.assertEquals(13, size);
 	}
 }
